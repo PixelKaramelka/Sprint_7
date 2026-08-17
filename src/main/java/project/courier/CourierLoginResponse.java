9a5ed34521a239dee6670f06e87d28532202e619
Nastya Zhuchkova <karamelkaaaa39@gmail.com>
2026-08-17 10:25:40 +0300

    Initial commit

---
+++ src/main/java/project/courier/CourierLoginResponse.java
@@ -0,0 +1,9 @@
+package project.courier;
+
+public class CourierLoginResponse {
+    private int id;
+    public int getId (){
+        return id;}
+    public void setId(int id){
+        this.id=id;}
+    }
