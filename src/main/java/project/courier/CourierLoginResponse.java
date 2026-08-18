2849b579ca4ea32f58b127163ebed3fa8cd5b3aa
Nastya Zhuchkova <karamelkaaaa39@gmail.com>
2026-08-18 07:30:04 +0300

    Initial empty main

--- src/main/java/project/courier/CourierLoginResponse.java
+++
@@ -1,9 +0,0 @@
-package project.courier;
-
-public class CourierLoginResponse {
-    private int id;
-    public int getId (){
-        return id;}
-    public void setId(int id){
-        this.id=id;}
-    }
